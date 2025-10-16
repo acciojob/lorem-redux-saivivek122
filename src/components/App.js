3c7110a077e28323998b50974b5226ac5fe18a70
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-16 07:17:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,24 @@ import { fetchLorem } from "./loremSlice";
 import './../styles/App.css';    
   
 const App = () => { 
-  const dispatch = useDispatch(); 
+ const dispatch = useDispatch();
   const { data, loading, error } = useSelector((state) => state.lorem);
 
-  useEffect(() => {  
+  useEffect(() => {
     dispatch(fetchLorem());
   }, [dispatch]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <h4>Loading...</h4>;
   if (error) return <p>Error: {error}</p>;
   if (!data) return null;
   return (
     <div>
         {/* Do not remove the main div */}
-       <h2>{data.title}</h2>
-      <p>{data.body}</p>
+       <h1>Welcome to Lorem Redux</h1> 
+      <ul>
+        <li>{data.title}</li>         
+        <li>{data.body}</li>
+      </ul>
     </div>
   )
 }

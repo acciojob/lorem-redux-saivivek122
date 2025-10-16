df337b2447e476c3cf5ad3602fab16f8f3d9878a
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-16 06:57:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLorem } from "./loremSlice";
 import './../styles/App.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector((state) => state.lorem);
+
+  useEffect(() => {
+    dispatch(fetchLorem());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!data) return null;
   return (
     <div>
         {/* Do not remove the main div */}
+       <h2>{data.title}</h2>
+      <p>{data.body}</p>
     </div>
   )
 }

03e96cfae35fd6aa37e9ff23165ccd4b620837cb
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-16 07:05:12 +0530

    Fix import formatting in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";  
 import { fetchLorem } from "./loremSlice";
 import './../styles/App.css';
 
